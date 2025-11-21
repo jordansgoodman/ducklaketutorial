ec6632d0957ddcab13f1d5bf1b8d8e4b5415d104
jordansgoodman <jordansamuelgoodman@protonmail.com>
2025-11-20 19:07:24 -0600

    update' '

--- queries.sql
+++ queries.sql
@@ -9,6 +9,12 @@ ATTACH 'duckdb:ducklake_meta.db'
 
 
 -- 2. Query specific version (replace N with actual version)
-SELECT *
+SELECT count(*) as count, '2' as version
 FROM my_ducklake.lake.who_ambient_air_quality_2024
-AT (VERSION => 3);
+AT (VERSION => 2)
+
+union all
+
+SELECT count(*) as count, '3' as version
+FROM my_ducklake.lake.who_ambient_air_quality_2024
+AT (VERSION => 3)
